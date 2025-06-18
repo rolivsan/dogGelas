72e69de86e9805e0bd0808a003b26d08d71c7a31
GustavoOliSantos <gustavo.oliveyra.santos@gmail.com>
2025-06-17 22:36:12 -0300

    add atributos na entidade

--- src/main/java/br/com/dog/entities/Dog.java
+++ src/main/java/br/com/dog/entities/Dog.java
@@ -2,6 +2,44 @@ package br.com.dog.entities;
 
 public class Dog {
 
+    private String name;
+    private String description;
+    private int frequency;
+    private String characteristcs;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getFrequency(){
+        return frequency;
+    }
+
+    public String getcharacter() {
+        return characteristcs;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setDescription(String description){
+        this.name=name;
+    }
+
+    public void setFrequency(int frequency){
+        this.frequency=frequency;
+    }
+
+    public void setCharacteristcs( String characteristcs){
+        this.frequency=frequency;
+    }
+
+
     //TODO CRIAR ATRIBUTOS, METODOS GET SET PARA NOSSA ENTIDADE NO CASO DOG
 
 }
